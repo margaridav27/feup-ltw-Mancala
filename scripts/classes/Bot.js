1ba35689c1f19b37aa9fb23f1f3439583851788f
Margarida Vieira <margaridajcv.27@gmail.com>
2021-12-19 19:01:05 +0000

    starting bot

--- scripts/classes/Bot.js
+++ scripts/classes/Bot.js
@@ -1,28 +1,93 @@
 class Bot {
-  constructor() {
-
-  }
-
   // simula jogada para calcular pontos que poderão ser obtidos com ela
-  simulateMoveExecution() {
+  simulateMoveExecution(house, board) {
+    let res = {pointsMove: -1, boardMove: -1};
+    let currentHole = house;
+    let holes = board.getHoles();
+    const seeds = holes[house];
+    const nrHoles = this.board.getNrHoles();
+    holes[house] = 0;
+    currentHole++;
+    for (let i = seeds; i > 0; i--) {
+      let lastSeed = (i - 1 == 0);
+      
+      holes[currentHole]++;
+      currentHole = ((nrHoles * 2) + (currentHole - 1)) % (nrHoles * 2);
     
+      if (lastSeed && holes[currentHole] == 0) 
+      res = { lastSowingOnWarehouse: false, lastSowingOnHole: true, lastSowing: hid, };
+      
+    }
+
+    return res;
+  }
+            
+            // return true;
+        
+    //  if (this.isValidMove(playedHole)) {
+    //   let res = this.sow(playedHole);
+
+    //   this.score[this.currentPlayer] = res.score;
+    //   this.updateScore();
+
+    //   if (res.lastSowingOnHole && this.sowedInOwnHole(res.lastSowing))   // last sowing occured in one of the current player's holes
+    //       this.score[this.currentPlayer] = this.capture(res.lastSowing);
+    //   else if (!res.lastSowingOnWarehouse)                               // last sowing did not occur in the current player's warehouse
+    //       this.setCurrentPlayer();                                       // swap players normally
+                                                          
+    //   if (!this.setValidMoves()) 
+    //       this.endGame();
+    // }
+  
+  
+  anticipateOpponentsBestMove(turn, board) {
+    let pointsBestMove = 0;
+    let bestMove = -1;
+    let boardBestMove = board;
+    const validMoves = turn == 0 ? [...Array(nrHoles).keys()].map(i => i + 0) : 
+                                   [...Array(nrHoles).keys()].map(i => i + nrHoles);
+
+    for (move in validMoves) {
+      const result = simulateMoveExecution(holes[i], board, turn);
+
+      if (result.pointsMove > pointsBestMove) {
+        bestMove = move;
+        pointsBestMove = result.pointsMove;
+        boardBestMove = result.boardMove;
+      }
+    }
+    
+    return { bestMove: bestMove, boardBestMove: boardBestMove }; 
   }
 
-  // antecipa melhor jogada do oponente
-  anticipateOpponentsBestMove() {
+  calculateBestMoveRec(level, currentLevel, turn, bestMove, pointsBestMove, board, boardBestMove) {
+    const nrHoles = board.getNrHoles();
+    const validMoves = turn == 0 ? [...Array(nrHoles).keys()].map(i => i + 0) : 
+                                   [...Array(nrHoles).keys()].map(i => i + nrHoles);
+
+    for (move in validMoves) {
+      const result = simulateMoveExecution(move, board, turn);
+
+      if (result.pointsMove > pointsBestMove) {
+        bestMove = move;
+        pointsBestMove = result.pointsMove;
+        boardBestMove = result.boardMove;
+      }
+    }
+
+    if (currentLevel == level) 
+      return { bestMove: bestMove, boardBestMove: boardBestMove };
+
+    const opponentsTurn = turn == 0 ? 1 : 0;
+    const opponentResult = this.anticipateOpponentsBestMove(opponentsTurn, boardBestMove)
     
+    calculateBestMoveRec(level, currentLevel++, turn, bestMove, pointsBestMove, opponentResult.boardBestMove, boardBestMove);
+  }
+ 
+  calculateBestMove(level, turn, board) {
+    return this.calculateBestMoveRec(level, turn, 1, -1, board, board);
   }
 
-  // calcula melhor jogada - main function
-  calculateBestMove() {
-    
-  }
-
-  // calcula melhor jogada - recursive function
-  calculateBestMoveRec() {
-    
-  }
-}
 
 /*
 CalcularMelhorJogada(nível, board) {
