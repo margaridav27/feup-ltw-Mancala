3f34b71670e1bff7be7cc765d7cadc3a26751ab7
Margarida Vieira <margaridajcv.27@gmail.com>
2022-01-06 15:37:26 +0000

    board displayer clean up

--- scripts/classes/BoardDisplayer.js
+++ scripts/classes/BoardDisplayer.js
@@ -13,7 +13,6 @@ class BoardDisplayer {
     this.constructSeeds(data.seeds);
   }
 
-  /* -------------------- DIMENSIONS & POSITIONS -------------------- */
   getBoardDimensions() {
     const totalWidth = window.innerWidth;
     const totalHeight = window.innerHeight;
@@ -22,43 +21,44 @@ class BoardDisplayer {
     return { w: boardWidth, h: boardHeight };
   }
 
+  getWarehouseDimensions() {
+    const warehouseWidth = Math.floor((1 / 6) * this.boardDimensions.w);
+    const warehouseHeight = this.boardDimensions.h;
+    return { w: warehouseWidth, h: warehouseHeight };
+  }
+
   getHoleDimensions() {
-    const boardWidth = this.boardDimensions.w;
-    const boardHeight = this.boardDimensions.h;
-    const holeWidth = Math.floor(((2 / 3) * boardWidth) / this.nrHolesEachSide);
-    const holeHeight = Math.floor((1 / 2) * boardHeight);
+    const holeWidth = Math.floor(((2 / 3) * this.boardDimensions.w) / this.nrHolesEachSide);
+    const holeHeight = Math.floor((1 / 2) * this.boardDimensions.h);
     return { w: holeWidth, h: holeHeight };
   }
 
-  getWarehouseDimensions() {
-    const boardWidth = this.boardDimensions.w;
-    const boardHeight = this.boardDimensions.h;
-    const whWidth = Math.floor((1 / 6) * boardWidth);
-    const whHeight = Math.floor(boardHeight);
-    return { w: whWidth, h: whHeight };
+  positionSeedRandomlyInHole(seed) {
+    const marginOfError = 30;
+    const seedRadius = this.holeDimensions.w / 14;
+    const holeCentered = Math.floor(this.holeDimensions.w / 2 - seedRadius);
+    const maxDelta = holeCentered - marginOfError;
+    const offset = { l: random(-maxDelta, maxDelta), t: random(-maxDelta, maxDelta) };
+    seed.style.left = `${holeCentered + offset.l}px`;
+    seed.style.top = `${holeCentered + offset.t}px`;
   }
 
-  getHoleTopLeftOffsets(hid) {
-    const warehouseWidth = this.warehouseDimensions.w;
-    const holeWidth = this.holeDimensions.w;
-    const holeHeight = this.holeDimensions.h;
-    let leftOffset = warehouseWidth + holeWidth * (Math.abs(hid - this.nrHolesEachSide) - 1);
-    let topOffset = hid < this.nrHolesEachSide ? 0 : holeHeight;
-    return { l: leftOffset, t: topOffset };
-  }
-
-  getSeedTopLeftOffsets(seed) {
-    return {
-      t: Math.floor(parseInt(seed.style.top.slice(0, -2))),
-      l: Math.floor(parseInt(seed.style.left.slice(0, -2))),
+  positionSeedRandomlyInWarehouse(seed) {
+    const marginOfError = 30;
+    const seedRadius = this.holeDimensions.w / 14;
+    const warehouseCentered = {
+      l: Math.floor(this.warehouseDimensions.w / 2 - seedRadius),
+      t: Math.floor(this.warehouseDimensions.h / 2 - seedRadius),
     };
+    const maxDelta = {
+      l: warehouseCentered.l - marginOfError,
+      t: warehouseCentered.t - marginOfError,
+    };
+    const offset = { l: random(-maxDelta.l, maxDelta.l), t: random(-maxDelta.t, maxDelta.t) };
+    seed.style.left = `${warehouseCentered.l + offset.l}px`;
+    seed.style.top = `${warehouseCentered.t + offset.t}px`;
   }
 
-  equalPosition(pos1, pos2) {
-    return pos1[0] == pos2[0] && pos1[1] == pos2[1];
-  }
-
-  /* -------------------- CONSTRUCTORS -------------------- */
   constructWarehouseAndRows() {
     let board = document.querySelector('.board-panel');
     document.getElementById('board-panel').innerHTML = '';
@@ -137,9 +137,6 @@ class BoardDisplayer {
   constructSeeds(seeds) {
     const seedsColorPalette = ['#ffca3a', '#8ac926', '#1982c4', '#ff9f1c', '#ffffff'];
 
-    const holeWidth = this.holeDimensions.w;
-    const holeHeight = this.holeDimensions.h;
-
     seeds.forEach((seed) => {
       let holeElement = document.getElementById(`col-${seed.hid}`);
       let seedElement = document.createElement('div');
@@ -147,15 +144,11 @@ class BoardDisplayer {
       seedElement.className = 'seed';
 
       // seed dimensions
-      seedElement.style.width = `${holeWidth / 6}px`;
+      seedElement.style.width = `${this.holeDimensions.w / 7}px`;
       seedElement.style.height = seedElement.style.width;
 
       // position relatively to the hole
-      const marginOfError = 30;
-      const offsetLeft = random(marginOfError, holeWidth - marginOfError);
-      const offsetTop = random(marginOfError, holeHeight - marginOfError);
-      seedElement.style.left = `${offsetLeft}px`;
-      seedElement.style.top = `${offsetTop}px`;
+      this.positionSeedRandomlyInHole(seedElement);
 
       // color
       const seedColor = seedsColorPalette[random(0, seedsColorPalette.length - 1)];
@@ -165,7 +158,6 @@ class BoardDisplayer {
     });
   }
 
-  /* -------------------- VISUAL UPDATES -------------------- */
   updateStatus(warehouses, holes, score, turn) {
     warehouses.forEach((warehouse) => {
       document.querySelector(`#wh-${warehouse.wid} span`).innerText = warehouse.value;
@@ -183,94 +175,35 @@ class BoardDisplayer {
     const info = document.getElementById('info');
     info.innerText =
       turn === 0
-        ? `It's ${this.nameP1}'s turn. Make your move.`
-        : `It's ${this.nameP2}'s turn. Make your move.`;
+        ? `It's ${this.nameP1}'s turn. Show us what you got!`
+        : `It's ${this.nameP2}'s turn. Show us what you got!`;
   }
 
   executePhases(sow, capture, cleaning) {
-    this.animation(sow)
-      .then(() => this.animation(capture))
-      .then(() => this.animation(cleaning));
+    this.moveSeeds(sow)
+      .then(() => this.moveSeeds(capture))
+      .then(() => this.moveSeeds(cleaning));
   }
 
-  async animation(moves) {
+  async moveSeeds(moves) {
     for (let move of moves) {
-      let board = document.getElementById('board-panel');
+      let seed = document.getElementById(`seed-${move.sid}`);
 
       let from = document.getElementById(`col-${move.from}`);
+      from.removeChild(seed);
 
       let to;
       if (move.to < 0) {
-        move.to = Math.abs(move.to) - 1;
-        to = document.getElementById(`wh-${move.to}`);
+        to = document.getElementById(`wh-${Math.abs(move.to) - 1}`);
+        to.appendChild(seed);
+        this.positionSeedRandomlyInWarehouse(seed);
       } else {
         to = document.getElementById(`col-${move.to}`);
+        to.appendChild(seed);
+        this.positionSeedRandomlyInHole(seed);
       }
 
-      let seed = document.getElementById(`seed-${move.sid}`);
-
-      const fromPos = this.getHoleTopLeftOffsets(move.from);
-      const originalSeedPos = this.getSeedTopLeftOffsets(seed);
-
-      seed.style.top = `${fromPos.t + originalSeedPos.t}px`;
-      seed.style.left = `${fromPos.l + originalSeedPos.l}px`;
-
-      // removes seed from the origin and transfers it to the board, to the same position visually
-      from.removeChild(seed);
-
-      board.appendChild(seed);
-
-      let toPos;
-      if (move.to < 0) {
-        // destination is a warehouse
-        if (move.to == -1) toPos = { t: 0, l: 0 };
-        else
-          toPos = {
-            t: 0,
-            l: this.getBoardDimensions().w - this.getWarehouseDimensions().w,
-          };
-      } else toPos = this.getHoleTopLeftOffsets(move.to);
-
-      // bezier curve values
-      let currSeedPos = this.getSeedTopLeftOffsets(seed);
-      let P1 = [currSeedPos.l, currSeedPos.t];
-      const P4 = [toPos.l + originalSeedPos.l, toPos.t + originalSeedPos.t];
-      const R1 = [0, 1];
-      const R4 = [0, -1];
-      let t = 0;
-
-      // moves seed along the board, from origin to destination
-      while (!this.equalPosition(P1, P4)) {
-        t += 0.1;
-        seed.style.left = `${
-          (2 * Math.pow(t, 3) - 3 * Math.pow(t, 2) + 1) * P1[0] +
-          (-2 * Math.pow(t, 3) + 3 * Math.pow(t, 2)) * P4[0] +
-          (Math.pow(t, 3) - 2 * Math.pow(t, 2) + t) *
-            R1[0] *
-            (Math.pow(t, 3) - Math.pow(t, 2)) *
-            R4[0]
-        }px`;
-        seed.style.top = `${
-          (2 * Math.pow(t, 3) - 3 * Math.pow(t, 2) + 1) * P1[1] +
-          (-2 * Math.pow(t, 3) + 3 * Math.pow(t, 2)) * P4[1] +
-          (Math.pow(t, 3) - 2 * Math.pow(t, 2) + t) *
-            R1[1] *
-            (Math.pow(t, 3) - Math.pow(t, 2)) *
-            R4[1]
-        }px`;
-
-        currSeedPos = this.getSeedTopLeftOffsets(seed);
-        P1 = [currSeedPos.l, currSeedPos.t];
-
-        await sleep(20);
-      }
-
-      // removes seed from the board and transfers it to the destination
-      board.removeChild(seed);
-
-      seed.style.top = `${originalSeedPos.t}px`;
-      seed.style.left = `${originalSeedPos.l}px`;
-      to.appendChild(seed);
+      await sleep(150);
     }
   }
 
@@ -281,6 +214,9 @@ class BoardDisplayer {
       data.status.score,
       data.status.turn
     );
-    this.executePhases(data.sow, data.capture, data.cleaning);
+    this.executePhases(
+      data.sow, 
+      data.capture, 
+      data.cleaning);
   }
 }
