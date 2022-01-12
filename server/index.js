1a616cac896d1297e4e46107a95ab49c68202f69
Margarida Vieira <margaridajcv.27@gmail.com>
2022-01-12 11:33:17 +0000

    register with encrypted pass

--- server/index.js
+++ server/index.js
@@ -5,26 +5,23 @@ const PORT = 9080;
 const http = require('http');
 const url = require('url');
 
-const fs = require('fs'); // to serve as database
-const crypto = require('crypto'); // to encrypt the players passwords
-
 const updater = require('./modules/updater.js');
 const register = require('./modules/register.js');
 const ranking = require('./modules/ranking.js');
 const game = require('./modules/game.js');
 
 const headers = {
-    plain: {
-        'Content-Type': 'application/javascript',
-        'Cache-Control': 'no-cache',
-        'Access-Control-Allow-Origin': '*'        
-    },
-    sse: {    
-        'Content-Type': 'text/event-stream',
-        'Cache-Control': 'no-cache',
-        'Access-Control-Allow-Origin': '*',
-        'Connection': 'keep-alive'
-    }
+  plain: {
+    'Content-Type': 'application/javascript',
+    'Cache-Control': 'no-cache',
+    'Access-Control-Allow-Origin': '*',
+  },
+  sse: {
+    'Content-Type': 'text/event-stream',
+    'Cache-Control': 'no-cache',
+    'Access-Control-Allow-Origin': '*',
+    Connection: 'keep-alive',
+  },
 };
 
 const server = http.createServer(function (request, response) {
@@ -32,25 +29,26 @@ const server = http.createServer(function (request, response) {
   const pathname = preq.pathname;
 
   switch (pathname) {
-    case '/register': 
-      register.register(preq, response); 
+    case '/register':
+      register.register(preq, response);
       break;
-    case '/join': 
-      game.join(); 
+    case '/join':
+      game.join();
       break;
-    case '/notify': 
-      game.notify(); 
+    case '/notify':
+      game.notify();
       break;
-    case '/update': 
-      game.update(); 
+    case '/update':
+      game.update();
       break;
-    case '/leave': 
-      game.leave(); 
+    case '/leave':
+      game.leave();
       break;
-    case '/ranking': 
-      ranking.ranking(response); 
+    case '/ranking':
+      ranking.ranking(response);
       break;
-    case '/favicon.ico': break;
+    case '/favicon.ico':
+      break;
     default:
       response.writeHead(400, { 'Content-Type': 'text/plain' });
       response.end({});
--- server/modules/ranking.js
+++ server/modules/ranking.js
@@ -1,30 +1,9 @@
-const fs = require('fs');
+const database = require('./database.js');
 
-function updateDatabase(value, table) {
-    fs.readFile('database/database.json', 'utf8', function readFileCallback(err, data){
-        if (err){
-            console.log("heloooo", err);
-        } else {
-
-        let updatable = JSON.parse(data)[table]; 
-        updatable.push(value);
-        json = JSON.stringify(updatable)
-
-        fs.writeFile('database/database.json', json, 'utf8', callback); // write it back 
-    }});
-}
-
-
-function readDatabase(table) {
-    const data = fs.readFileSync('database/database.json');
-    return JSON.parse(data)[table];
-}
-
-module.exports.ranking = function(response) {
-  const ranking = readDatabase('ranking');
-  const responseBody = { ranking: ranking };
+module.exports.ranking = function (response) {
+  const ranking = database.read('ranking');
 
   response.writeHead(200);
-  response.write(JSON.stringify(responseBody));
+  response.write(JSON.stringify({ ranking: ranking }));
   response.end();
-}
+};
